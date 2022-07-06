78a8339fdd5e16c185520a0e4fc99c6c090580f5
OzcanGuler <gulerozcan818@gmail.com>
2022-07-06 20:40:44 +0300

    keep updating

---
+++ TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        public int[] Grid { get; set; }
+
+        public Board()
+        {
+            Grid = new int[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Grid[i] = 0;
+            }
+        }
+        public bool isBoardFull()
+        {
+            bool isFull = true;
+            for (int i = 0; i < Grid.Length; i++)
+            {
+                if (Grid[i] == 0)
+                {
+                    isFull = false;
+                }
+            }
+            return isFull;
+        }
+        public int checkForWinner()
+        {
+            if (Grid[0] == Grid[1] && Grid[1] == Grid[2])
+            {
+                return Grid[0];
+            }
+            if (Grid[3] == Grid[4] && Grid[4] == Grid[5])
+            {
+                return Grid[3];
+            }
+            if (Grid[6] == Grid[7] && Grid[7] == Grid[8])
+            {
+                return Grid[6];
+            }
+            if (Grid[0] == Grid[4] && Grid[4] == Grid[8])
+            {
+                return Grid[0];
+            }
+            if (Grid[0] == Grid[3] && Grid[3] == Grid[6])
+            {
+                return Grid[0];
+            }
+            if (Grid[1] == Grid[4] && Grid[4] == Grid[7])
+            {
+                return Grid[1];
+            }
+            if (Grid[2] == Grid[5] && Grid[5] == Grid[8])
+            {
+                return Grid[2];
+            }
+
+            if (Grid[2] == Grid[4] && Grid[4] == Grid[6])
+            {
+                return Grid[2];
+            }
+            return 0;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        Board game = new Board();
+        Button[] buttons = new Button[9];
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            game = new Board();
+            buttons[0] = button1;
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
+            buttons[4] = button5;
+            buttons[5] = button6;
+            buttons[6] = button7;
+            buttons[7] = button8;
+            buttons[8] = button9;
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Click += handleButtonClick;
+                buttons[i].Tag = i;
+            }
+        }
+
+      
+        private void handleButtonClick(object sender, EventArgs e)
+        {
+
+
+            Button clickedButton = (Button)sender;
+           // MessageBox.Show("button " + clickedButton.Tag + " was clicked");
+
+            int gameSquareNumber = (int)clickedButton.Tag;
+            game.Grid[gameSquareNumber] = 1;
+            updateBoard();
+
+            if (game.isBoardFull() == true)
+            {
+                MessageBox.Show("The board is full");
+                disableAllbuttons();
+            }
+            else if (game.checkForWinner() == 1)
+            {
+                MessageBox.Show("Player human wins!");
+                disableAllbuttons();
+            }
+            else
+            {
+                computerChoose();
+            }
+            
+        }
+
+        private void disableAllbuttons()
+        {
+            foreach (var item in buttons)
+            {
+                item.Enabled = false;
+                item.BackColor = Color.Transparent;
+                item.Text = "";
+            }
+        }
+
+        private void computerChoose()
+        {
+            int computerTurn = r.Next(9);
+            //MessageBox.Show(computerTurn.ToString());
+            while (computerTurn==-1||game.Grid[computerTurn]!=0)
+            {
+                computerTurn = r.Next(9);
+                //MessageBox.Show(computerTurn.ToString());
+            }
+            game.Grid[computerTurn] = 2;
+            updateBoard();
+
+            if (game.isBoardFull() == true)
+            {
+                MessageBox.Show("The board is full");
+                disableAllbuttons();
+            }
+            else if (game.checkForWinner() == 2)
+            {
+                MessageBox.Show("Player computer wins!");
+                disableAllbuttons();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateBoard();
+        }
+
+        private void updateBoard()
+        {
+            for (int i = 0; i < game.Grid.Length; i++)
+            {
+                if (game.Grid[i] == 0)
+                {
+                    buttons[i].Text = "";
+                    buttons[i].Enabled = true;
+                }
+                else if (game.Grid[i] == 1)
+                {
+                    buttons[i].Text = "X";
+                    buttons[i].Enabled = false;
+                    buttons[i].BackColor = Color.Red;
+                }
+                else if (game.Grid[i] == 2)
+                {
+                    buttons[i].Text = "O";
+                    buttons[i].Enabled = false;
+                    buttons[i].BackColor = Color.Green;
+                }
+            }
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            game = new Board();
+            enableAllButtons();
+        }
+
+        private void enableAllButtons()
+        {
+            foreach (var item in buttons)
+            {
+                item.Enabled = true;
+            }
+            updateBoard();
+        }
+    }
+}
