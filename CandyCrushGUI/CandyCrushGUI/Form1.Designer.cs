0a13678404340d7cdcc1b2a6ed02924125bb71db
OzcanGuler <gulerozcan818@gmail.com>
2022-07-30 19:01:32 +0300

    Updated

--- CandyCrushGUI/CandyCrushGUI/Form1.Designer.cs
+++ CandyCrushGUI/CandyCrushGUI/Form1.Designer.cs
@@ -39,8 +39,13 @@
             this.button8 = new System.Windows.Forms.Button();
             this.btn_rst = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -149,29 +154,78 @@
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // label5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(611, 235);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Clicks:";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(749, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(716, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Clicks:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(716, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Player 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(611, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Player 1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(611, 265);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 173);
+            this.listBox1.TabIndex = 20;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(644, 235);
+            this.label2.Location = new System.Drawing.Point(644, 232);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(13, 13);
-            this.label2.TabIndex = 12;
+            this.label2.TabIndex = 19;
             this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(611, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Clicks:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -206,8 +260,13 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button btn_rst;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CandyCrushGUI/CandyCrushGUI/Form1.cs
+++ CandyCrushGUI/CandyCrushGUI/Form1.cs
@@ -16,17 +16,24 @@ namespace CandyCrushGUI
         private myButton[,] btnGrid;
         private Color currColor, origColor;
         private int rows, cols;
-        int i = 0;
+        int i = 0, j = 0, z = 0;
+        List<string> list = new List<string>();
+        int c = 0;
+        int p1, p2;
         public Form1()
         {
             InitializeComponent();
             populateGrid();
+
+            buttonCopy();
         }
         public void populateGrid()
         {
+            int x, y;
             //calculate the number of rows and cols based on the panel size and the button size.
             cols = panel1.Height / myButton.btn_size;
             rows = panel1.Width / myButton.btn_size;
+
             // new 2D array of buttons.
             btnGrid = new myButton[rows, cols];
 
@@ -38,57 +45,153 @@ namespace CandyCrushGUI
                     btnGrid[r, c] = new myButton();
                     btnGrid[r, c].row = r;
                     btnGrid[r, c].col = c;
-
-                    //assign the same event handler to every button in the panel.
+                    //if (z == 0)
+                    //{
                     btnGrid[r, c].Click += gridbutton_Click;
+                    //}
+                    //assign the same event handler to every button in the panel.
+                    //if(z==1)
+                    // {
+                    //     btnGrid[r, c].Click += gridbutton1_Click;
+                    // }
 
                     //add the button to the panel.
                     panel1.Controls.Add(btnGrid[r, c]);
                     //change the location of the button to its proper x y coordinates.
-                    btnGrid[r, c].Location = new Point(r * myButton.btn_size, c * myButton.btn_size);
+                    btnGrid[r, c].Location = new Point(r * myButton.btn_size, c * myButton.btn_size);       //butonları satır ve sütun numaralarına göre yeri belirlenir.
+
+
                 }
             }
+            //buttonCopy();
+        }
+        public void buttonCopy()
+        {
+
+            string ss;
+            char[] charsToTrim = { 'C', 'o', 'l', 'o', 'r', '[', ' ', ']' };
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    ss = btnGrid[i, j].BackColor.ToString();
+                    ss = ss.Trim(charsToTrim);
+                    list.Add(ss);
+                    //MessageBox.Show(btnGrid[i, j].BackColor.ToString());
+
+                    //btnGrid[0, 0].BackColor = Color.FromName("" + ss);
+
+                }
+            }
+            foreach (var item in list)
+            {
+                listBox1.Items.Add(item);
+            }
+            //btnGrid[0, 0].BackColor = Color.FromName("" + list[1]);
+            //MessageBox.Show(btnGrid[0,0].BackColor.ToString());
+
+
+            //MessageBox.Show(list[1].ToString());
+            //ss = btnGrid[0, 1].BackColor.ToString();
+            //ss = ss.Trim(charsToTrim);
+            //btnGrid[0, 0].BackColor = Color.FromName(""+ss);
+
+        }
+        public void buttonpaste()
+        {
+            for (int a = 0; a < rows; a++)
+            {
+                for (int b = 0; b < cols; b++)
+                {
+
+                    btnGrid[a, b].BackColor = Color.FromName("" + list[c]);
+                    c++;
+                    z = 1;
+                }
+            }
+
+        }
+
+        private void colorbutton_click(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+            currColor = button.BackColor;
+            pictureBox1.BackColor = currColor;
+
         }
 
         private void gridbutton_Click(object sender, EventArgs e)
         {
-            i++;
-            myButton button = (myButton)sender;
-            origColor = button.BackColor;
-            floodFill(button.row, button.col);
-            label2.Text = i.ToString();
+            if (z == 0)
+            {
+                i++;
+                myButton button = (myButton)sender;
+                origColor = button.BackColor;
+                floodFill(button.row, button.col);
+                label2.Text = i.ToString();
 
-            isFinish(rows - 1, cols - 1);
+                isFinish(rows - 1, cols - 1);
+            }
+
+            else
+            {
+
+                j++;
+                myButton button = (myButton)sender;
+                origColor = button.BackColor;
+                floodFill(button.row, button.col);
+                label5.Text = j.ToString();
+
+                isFinish(rows - 1, cols - 1);
+            }
+
+
+
+            //if(currColor== btnGrid[0, 0].BackColor&& currColor == btnGrid[0, 1].BackColor && currColor == btnGrid[0, 2].BackColor
+            //   && currColor == btnGrid[1, 0].BackColor && currColor == btnGrid[1, 1].BackColor && currColor == btnGrid[1, 2].BackColor
+            //   && currColor == btnGrid[2, 0].BackColor && currColor == btnGrid[2, 1].BackColor && currColor == btnGrid[2, 2].BackColor
+            //   && currColor == btnGrid[3, 0].BackColor && currColor == btnGrid[3, 1].BackColor && currColor == btnGrid[3, 2].BackColor
+            //   )
+            //{
+            //    MessageBox.Show("bittti");
+            //}
+
+
+            //if(btnGrid[i, j].BackColor != currColor)
+            //{
+            //    i=0;j = 0;
+            //}
+
+            //if (btnGrid[z, k].BackColor == currColor && k + 1 == cols && z+1 != rows)
+            //{
+            //    if (z == rows - 1 && k == cols - 1)
+            //    {
+            //        MessageBox.Show("You win");
+            //    }
+            //    z++;
+            //    if (z == rows - 1 && k == cols - 1)
+            //    {
+            //        MessageBox.Show("You win");
+            //    }
+            //}
+
         }
-
-        //if(currColor== btnGrid[0, 0].BackColor&& currColor == btnGrid[0, 1].BackColor && currColor == btnGrid[0, 2].BackColor
-        //   && currColor == btnGrid[1, 0].BackColor && currColor == btnGrid[1, 1].BackColor && currColor == btnGrid[1, 2].BackColor
-        //   && currColor == btnGrid[2, 0].BackColor && currColor == btnGrid[2, 1].BackColor && currColor == btnGrid[2, 2].BackColor
-        //   && currColor == btnGrid[3, 0].BackColor && currColor == btnGrid[3, 1].BackColor && currColor == btnGrid[3, 2].BackColor
-        //   )
+        //private void gridbutton1_Click(object sender, EventArgs e)
         //{
-        //    MessageBox.Show("bittti");
+
+        //    j++;
+        //    myButton button = (myButton)sender;
+        //    origColor = button.BackColor;
+        //    floodFill(button.row, button.col);
+        //    label5.Text = j.ToString();
+
+        //    isFinish(rows - 1, cols - 1);
+
+
         //}
-
-
-        //if(btnGrid[i, j].BackColor != currColor)
-        //{
-        //    i=0;j = 0;
-        //}
-
-        //if (btnGrid[z, k].BackColor == currColor && k + 1 == cols && z+1 != rows)
-        //{
-        //    if (z == rows - 1 && k == cols - 1)
-        //    {
-        //        MessageBox.Show("You win");
-        //    }
-        //    z++;
-        //    if (z == rows - 1 && k == cols - 1)
-        //    {
-        //        MessageBox.Show("You win");
-        //    }
-        //}
-
 
 
         private void floodFill(int r, int c)
@@ -121,16 +224,18 @@ namespace CandyCrushGUI
         {
             panel1.Controls.Clear();
             label2.Text = "0";
+            label5.Text = "0";
             i = 0;
+            c = 0;
+            j = 0;
+            z = 0;
+
+            listBox1.Items.Clear();
             populateGrid();
+            //buttonCopy();
         }
 
-        private void colorbutton_click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currColor = button.BackColor;
-            pictureBox1.BackColor = currColor;
-        }
+      
 
         private bool isValid(int r, int c)
         {
@@ -145,7 +250,6 @@ namespace CandyCrushGUI
         }
         private void isFinish(int a, int b)
         {
-
             if (b == -1)
             {
                 a--;
@@ -154,15 +258,47 @@ namespace CandyCrushGUI
             }
             else if (a < 0)
             {
-                MessageBox.Show("Congratulations. Game is over");
-                for (int i = 0; i < rows; i++)
+                if (z == 0)
                 {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        btnGrid[i, j].Enabled = false;
-                    }
+                    MessageBox.Show("Player 1. Game is over");
                 }
+                else
+                {
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            btnGrid[i, j].Enabled = false;
+                        }
+                    }
+
+                    MessageBox.Show("Player 2. Game is over");
+
+                    p1 = Convert.ToInt32(label2.Text);
+                    p2 = Convert.ToInt32(label5.Text);
+                    if (p1 < p2)
+                    {
+                        MessageBox.Show("Player 1 Win");
+
+                    }
+                    else if (p2 < p1)
+                    {
+                        MessageBox.Show("Player 2 Win");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("It's Draw");
+
+                    }
+
+                }
+
+                c = 0;
+                buttonpaste();
+                //z = 1;
             }
+
             else if (b < 0)
             {
                 a--;
@@ -182,5 +318,52 @@ namespace CandyCrushGUI
             }
 
         }
+
+
+
+        /*  original method
+                private void isFinish(int a, int b)
+                {
+
+                    if (b == -1)
+                    {
+                        a--;
+                        b = cols - 1;
+                        isFinish(a, b);
+                    }
+                    else if (a < 0)
+                    {
+                        MessageBox.Show("Congratulations. Game is over");
+                        for (int i = 0; i < rows; i++)
+                        {
+                            for (int j = 0; j < cols; j++)
+                            {
+                                btnGrid[i, j].Enabled = false;
+                            }
+                        }
+                        buttonpaste();
+                    }
+                    else if (b < 0)
+                    {
+                        a--;
+                        b = cols - 1;
+                        isFinish(a, b);
+                    }
+                    else if (b > -1 && btnGrid[a, b].BackColor == currColor)
+                    {
+                        //if (a == 0 && b == 0 )
+                        //{
+                        //    MessageBox.Show("oyun bitti");
+                        //}
+                        b--;
+
+                        isFinish(a, b);
+
+                    }
+
+                }
+                */
+
+
     }
 }
