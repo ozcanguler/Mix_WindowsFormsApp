e60386e66896ae74f336f4b195b9f19ce777a06a
OzcanGuler <gulerozcan818@gmail.com>
2022-08-05 01:54:17 +0300

    keep updating

---
+++ WhackMole/WhackMole/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WhackMole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_target = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Point:";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(15, 115);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.Btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(15, 154);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.Btn_stop_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(15, 193);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 5;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.Btn_reset_Click);
+            // 
+            // btn_target
+            // 
+            this.btn_target.BackgroundImage = global::WhackMole.Properties.Resources.smiley;
+            this.btn_target.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_target.FlatAppearance.BorderSize = 0;
+            this.btn_target.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_target.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_target.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_target.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_target.Location = new System.Drawing.Point(15, 243);
+            this.btn_target.Name = "btn_target";
+            this.btn_target.Size = new System.Drawing.Size(47, 33);
+            this.btn_target.TabIndex = 6;
+            this.btn_target.UseVisualStyleBackColor = true;
+            this.btn_target.Click += new System.EventHandler(this.Btn_target_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_target);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Btn_target_miss_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_target;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WhackMole/WhackMole/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhackMole
+{
+   
+    public partial class Form1 : Form
+    {
+        private Stopwatch watch = new Stopwatch();
+        private Random rnd = new Random();
+        private int timerIntervale = 1000;
+        public int hits = 0;
+        private bool btnStopWasClicked = false;
+        public Form1()
+        {
+            InitializeComponent();
+            watch.Start();
+            timer1.Interval = timerIntervale;
+        }
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+            watch.Start();
+            timer1.Start();
+            timer1.Interval = timerIntervale;
+            btnStopWasClicked = false;
+            btn_target.Enabled = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = watch.Elapsed.ToString();
+            btn_target.Top = rnd.Next(0, Height - btn_target.Height * 3);
+            btn_target.Left = rnd.Next(btn_start.Location.X + btn_target.Width, Width - btn_target.Width);
+            
+        }
+    
+
+        private void Btn_stop_Click(object sender, EventArgs e)
+        {
+            watch.Stop();
+            //btn_target.Location = new Point(0, 0);
+            timer1.Stop();
+            btn_target.Enabled = false;
+            btnStopWasClicked = true;
+        }
+
+        private void Btn_reset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            watch.Stop();
+            watch.Reset();
+            label1.Text = "";
+            label2.Text = "0";
+            btn_target.Enabled = false;
+            btn_target.Location = new Point(0, 0);
+            btnStopWasClicked = true;
+            hits = 0;
+        }
+
+        private void Btn_target_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(btn_start.Location.X.ToString());
+            //MessageBox.Show(Width.ToString());
+            if (btn_target.Enabled == true)
+            {
+                hits++;
+                label2.Text = hits.ToString();
+                if (hits == 10)
+                {
+                    reset();
+                    MessageBox.Show("Congratulations");
+
+                }
+            }
+        }
+        private void Btn_target_miss_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(btn_start.Location.X.ToString());
+            //MessageBox.Show(Width.ToString());
+            if (btnStopWasClicked == false)
+            {
+                hits--;
+                label2.Text = hits.ToString();
+                if (hits < 0)
+                {
+                    reset();
+                    MessageBox.Show("Game Over");
+
+                }
+
+            }
+
+        }
+        public void reset()
+        {
+            timer1.Stop();
+            watch.Stop();
+            watch.Reset();
+            label1.Text = "";
+            label2.Text = "0";
+            btn_target.Enabled = false;
+            btn_target.Location = new Point(0, 0);
+            btnStopWasClicked = true;
+            hits = 0;
+        }
+    }
+}
